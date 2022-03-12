26f7df5fb7fd062eccf251fe132d5b5e7b4e579e
Marco Cesarato <cesarato.developer@gmail.com>
2022-03-12 19:08:19 +0100

    refactor: move assets to public folder

--- src/layouts/Main/PanelContainer.tsx
+++ src/layouts/Main/PanelContainer.tsx
@@ -1,11 +1,25 @@
-import {Box, useStyleConfig} from "@chakra-ui/react";
+import {Box, useColorModeValue as mode, useStyleConfig} from "@chakra-ui/react";
 
 function PanelContainer(props): JSX.Element {
 	const {variant, children, ...rest} = props;
 	const styles = useStyleConfig("PanelContainer", {variant});
 	// Pass the computed styles into the `__css` prop
 	return (
-		<Box __css={styles} {...rest} boxShadow="xl">
+		<Box
+			__css={styles}
+			{...rest}
+			boxShadow="xl"
+			_before={mode("", {
+				content: "''",
+				width: "100%",
+				height: "100%",
+				position: "absolute",
+				left: 0,
+				top: 0,
+				backgroundImage: "url(assets/images/noise.gif)",
+				zIndex: -1,
+				opacity: 0.05,
+			})}>
 			{children}
 		</Box>
 	);
