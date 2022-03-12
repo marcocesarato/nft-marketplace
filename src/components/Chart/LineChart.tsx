1dd610d7f500b280c9b225cf15e28c1029e8bf12
Marco Cesarato <cesarato.developer@gmail.com>
2022-03-12 18:31:26 +0100

    feat: add line chart component

---
+++ src/components/Chart/LineChart.tsx
@@ -0,0 +1,74 @@
+import ReactApexChart from "react-apexcharts";
+
+const chartOptions = {
+	chart: {
+		toolbar: {
+			show: false,
+		},
+	},
+	tooltip: {
+		theme: "dark",
+	},
+	dataLabels: {
+		enabled: false,
+	},
+	stroke: {
+		curve: "smooth",
+	},
+	xaxis: {
+		type: "datetime",
+		categories: [
+			"Jan",
+			"Feb",
+			"Mar",
+			"Apr",
+			"May",
+			"Jun",
+			"Jul",
+			"Aug",
+			"Sep",
+			"Oct",
+			"Nov",
+			"Dec",
+		],
+		labels: {
+			style: {
+				colors: "#c8cfca",
+				fontSize: "12px",
+			},
+		},
+	},
+	yaxis: {
+		labels: {
+			style: {
+				colors: "#c8cfca",
+				fontSize: "12px",
+			},
+		},
+	},
+	legend: {
+		show: false,
+	},
+	grid: {
+		strokeDashArray: 5,
+	},
+	fill: {
+		type: "gradient",
+		gradient: {
+			shade: "light",
+			type: "vertical",
+			shadeIntensity: 0.5,
+			gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
+			inverseColors: true,
+			opacityFrom: 0.8,
+			opacityTo: 0,
+			stops: [],
+		},
+		colors: ["#4FD1C5", "#2D3748"],
+	},
+	colors: ["#4FD1C5", "#2D3748"],
+};
+
+export default function LineChart({chartData}): JSX.Element {
+	return <ReactApexChart options={chartOptions} series={chartData} type="line" height={350} />;
+}
