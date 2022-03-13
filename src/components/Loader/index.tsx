766522b1e39f1c024996dd09cf8075247dccf3bb
Marco Cesarato <marco.cesarato@accenture.com>
2022-03-13 16:06:58 +0100

    fix: add typing to loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,6 +1,7 @@
+import {ReactNode} from "react";
 import {Center, Spinner, Text} from "@chakra-ui/react";
 
-export default function Loader({message}): JSX.Element {
+export default function Loader({message}: {message?: string | ReactNode}): JSX.Element {
 	return (
 		<Center flex="1" flexDirection="column">
 			<Spinner thickness="5px" speed="0.65s" color="main" size="xl" />
