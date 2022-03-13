8aa719182c2facf2fd3ee8efb185f123fb77d3a0
Marco Cesarato <cesarato.developer@gmail.com>
2022-03-13 01:32:34 +0100

    feat: add particles header on home

---
+++ src/components/Particles/index.tsx
@@ -0,0 +1,75 @@
+import {useMemo, useRef} from "react";
+import {Canvas, useFrame} from "@react-three/fiber";
+import * as THREE from "three";
+import {InstancedMesh} from "three";
+
+function Swarm({count}) {
+	const mesh = useRef();
+	const light = useRef();
+
+	const dummy = useMemo(() => new THREE.Object3D(), []);
+	const particles = useMemo(() => {
+		const temp = [];
+		for (let i = 0; i < count; i++) {
+			const t = Math.random() * 100;
+			const factor = 20 + Math.random() * 10;
+			const speed = 0.01 + Math.random() / 200;
+			const xFactor = -50 + Math.random() * 100;
+			const yFactor = -50 + Math.random() * 100;
+			const zFactor = -50 + Math.random() * 100;
+			temp.push({t, factor, speed, xFactor, yFactor, zFactor, mx: 0, my: 0});
+		}
+		return temp;
+	}, [count]);
+	useFrame((state) => {
+		if (!light?.current || !mesh?.current) return;
+		particles.forEach((particle, i) => {
+			let {t, factor, speed, xFactor, yFactor, zFactor} = particle;
+			t = particle.t += speed / 2;
+			const a = Math.cos(t) + Math.sin(t * 1) / 10;
+			const b = Math.sin(t) + Math.cos(t * 2) / 10;
+			const s = Math.cos(t);
+			dummy.position.set(
+				(particle.mx / 10) * a +
+					xFactor +
+					Math.cos((t / 10) * factor) +
+					(Math.sin(t * 1) * factor) / 10,
+				(particle.my / 10) * b +
+					yFactor +
+					Math.sin((t / 10) * factor) +
+					(Math.cos(t * 2) * factor) / 10,
+				(particle.my / 10) * b +
+					zFactor +
+					Math.cos((t / 10) * factor) +
+					(Math.sin(t * 3) * factor) / 10,
+			);
+			dummy.scale.set(s, s, s);
+			dummy.rotation.set(s * 5, s * 5, s * 5);
+			dummy.updateMatrix();
+			// And apply the matrix to the instanced item
+			(mesh.current as InstancedMesh).setMatrixAt(i, dummy.matrix);
+		});
+		(mesh.current as InstancedMesh).instanceMatrix.needsUpdate = true;
+	});
+	return (
+		<>
+			<pointLight ref={light} distance={40} intensity={8} color="lightblue" />
+			<instancedMesh ref={mesh} args={[null, null, count]}>
+				<sphereGeometry attach="geometry" args={[0.2, 0]} />
+				<meshPhongMaterial attach="material" color="#805ad5" />
+			</instancedMesh>
+		</>
+	);
+}
+
+export default function Particles({style = {}}) {
+	return (
+		<>
+			<Canvas camera={{fov: 100, position: [0, 0, 30]}} style={style}>
+				<fog attach="fog" args={["#805ad5", 50, 190]} />
+				<pointLight distance={100} intensity={4} color="white" />
+				<Swarm count={500} />
+			</Canvas>
+		</>
+	);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,35 @@
 import Link from "next/link";
-import {Box, Button, Heading, Img, LightMode, Stack, Text} from "@chakra-ui/react";
+import {Box, Button, Heading, Stack, Text} from "@chakra-ui/react";
+
+import Catalog from "@components/Catalog";
+import Loading from "@components/Loading";
+import Particles from "@components/Particles";
+import Product from "@components/Product";
+import useMarketItems from "@hooks/useMarketItems";
 
 export default function Home(): JSX.Element {
+	const {data, isLoading} = useMarketItems();
 	return (
 		<Box>
-			<Box as="section" py="7.5rem">
-				<Box maxW={{base: "xl", md: "5xl"}} mx="auto" px={{base: "6", md: "8"}}>
-					<Box textAlign="center">
+			<Box as="section" pb="7.5rem">
+				<Box mx="auto" px={6}>
+					<Box
+						position="absolute"
+						top="94px"
+						width="calc(100% - 30px)"
+						left="15px"
+						bg="rgba(0,0,0,0.1)"
+						borderRadius="15px"
+						borderBottomLeftRadius={0}
+						borderBottomRightRadius={0}>
+						<Particles
+							style={{
+								height: "50vh",
+								filter: "blur(2px)",
+							}}
+						/>
+					</Box>
+					<Box textAlign="center" marginTop={{base: "50px", md: "100px"}}>
 						<Heading
 							as="h1"
 							size="3xl"
@@ -22,37 +45,37 @@ export default function Home(): JSX.Element {
 							home in real time and interact with them.
 						</Text>
 					</Box>
-
 					<Stack
 						justify="center"
 						direction={{base: "column", md: "row"}}
 						mt="10"
-						mb="20"
+						mb="10"
 						spacing="4">
-						<LightMode>
-							<Link href="/explore" passHref>
-								<Button
-									size="lg"
-									colorScheme="purple"
-									px="8"
-									fontWeight="bold"
-									fontSize="md">
-									Explore the marketplace
-								</Button>
-							</Link>
-						</LightMode>
+						<Link href="/explore" passHref>
+							<Button
+								size="lg"
+								colorScheme="purple"
+								px="8"
+								fontWeight="bold"
+								fontSize="md">
+								Explore the marketplace
+							</Button>
+						</Link>
+						<Link href="/sell" passHref>
+							<Button size="lg" px="8" fontWeight="bold" fontSize="md">
+								Put your art on the market
+							</Button>
+						</Link>
 					</Stack>
-					<Box
-						className="group"
-						cursor="pointer"
-						position="relative"
-						rounded="lg"
-						overflow="hidden">
-						<Img
-							alt="Screenshot"
-							src="https://images.unsplash.com/photo-1609423433459-a65b330ef5da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-						/>
-					</Box>
+					{isLoading ? (
+						<Loading />
+					) : (
+						<Catalog>
+							{data.map((nft, i) => (
+								<Product key={i} data={nft} />
+							))}
+						</Catalog>
+					)}
 				</Box>
 			</Box>
 		</Box>
