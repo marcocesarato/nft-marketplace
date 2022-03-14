8518eed43a17644e8517daa7061edee87f9dea56
Marco Cesarato <marco.cesarato@accenture.com>
2022-03-14 14:59:26 +0100

    chore: remove noise effect on dark mode

--- src/layouts/Main/PanelContainer.tsx
+++ src/layouts/Main/PanelContainer.tsx
@@ -1,25 +1,11 @@
-import {Box, useColorModeValue as mode, useStyleConfig} from "@chakra-ui/react";
+import {Box, useStyleConfig} from "@chakra-ui/react";
 
 function PanelContainer(props): JSX.Element {
 	const {variant, children, ...rest} = props;
 	const styles = useStyleConfig("PanelContainer", {variant});
 	// Pass the computed styles into the `__css` prop
 	return (
-		<Box
-			__css={styles}
-			{...rest}
-			boxShadow="xl"
-			_before={mode("", {
-				content: "''",
-				width: "100%",
-				height: "100%",
-				position: "absolute",
-				left: 0,
-				top: 0,
-				backgroundImage: "url(./assets/images/noise.gif)",
-				zIndex: -1,
-				opacity: 0.05,
-			})}>
+		<Box __css={styles} {...rest} boxShadow="xl">
 			{children}
 		</Box>
 	);
