d296e978a324852450d15c1fde135dee1dc5bc02
Marco Cesarato <marco.cesarato@accenture.com>
2022-03-20 13:43:39 +0100

    fix: sync ipfs data node to the graph node

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import {CID, create as createIpfsClient} from "ipfs-http-client";
 import mime from "mime-types";
 import type {File} from "multiparty";
 import type {NextApiResponse} from "next";
@@ -16,24 +17,96 @@ const handler = nextConnect<NextApiRequestFiles, NextApiResponse>()
 		try {
 			const metadaUrl = await storeToIPFS(files.file[0], body.name[0], body.description[0]);
 			return response.status(200).json({
-				url: metadaUrl.url,
+				url: metadaUrl,
 			});
 		} catch (error) {
 			console.error("Error uploading file: ", error);
 		}
 	});
 
+const syncWait = (ms) => {
+	return new Promise((resolve, _reject) => {
+		setTimeout(() => {
+			resolve(ms);
+		}, ms);
+	});
+};
+
+const fetchData = async ({fromClient, cid, retries = 3, retryWait = 500}) => {
+	try {
+		return await fromClient.cat(cid);
+	} catch (e) {
+		if (e.message.match("dag node is a directory")) {
+			console.info(`Skipping file: File is a directory`);
+		} else if (retries > 0) {
+			console.info(`Failed to retrieve file: Retrying...`);
+			await syncWait(10000);
+			return await fetchData({
+				fromClient,
+				cid,
+				retries: retries - 1,
+				retryWait,
+			});
+		} else {
+			console.warn(`Failed to retrieve file: ${e.message}`);
+			throw new Error("Max retries reached.");
+		}
+	}
+};
+
 async function storeToIPFS(data: File, name: string, description: string) {
+	// Upload to IPFS
 	const content = await fs.promises.readFile(data.path);
 	const type = mime.lookup(data.path);
 	const image = new FileStore([content], path.basename(data.path), {type});
 
 	const nftstorage = new NFTStorage({token: process.env.IPFS_API_KEY});
-	return await nftstorage.store({
+	const result = await nftstorage.store({
 		image,
 		name,
 		description,
 	});
+
+	// Sync CID with The Graph IPFS node for the indexing
+	const cid = CID.parse(result.ipnft);
+
+	let fromClient = createIpfsClient({
+		host: "nftstorage.link",
+		apiPath: "/ipfs/",
+		timeout: 10000,
+	});
+
+	let toClient = createIpfsClient({
+		host: "api.thegraph.com",
+		apiPath: "/ipfs/",
+		timeout: 10000,
+	});
+
+	let syncData: any;
+	try {
+		syncData = await fetchData({
+			fromClient,
+			cid,
+		});
+	} catch (e) {
+		console.warn(`Failed to retrieve files: ${e.message}`);
+		return result.url;
+	}
+
+	// Upload file
+	let targetFile: any;
+	try {
+		targetFile = await toClient.addAll(syncData);
+	} catch (e) {
+		console.error(`Failed to upload and sync file: ${e.message}`);
+		return result.url;
+	}
+
+	// Verify integrity before and after
+	if (!cid.equals(cid)) {
+		console.error(`Failed to sync file: Uploaded file cid differs: ${targetFile.cid}`);
+	}
+	return result.url;
 }
 
 export const config = {
