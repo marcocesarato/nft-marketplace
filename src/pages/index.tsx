386670b92d6c702a30e0e992d79c3a19c219f6be
Marco Cesarato <marco.cesarato@accenture.com>
2022-03-21 17:28:58 +0100

    fix: add illustration on home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
 import Link from "next/link";
-import {Box, Button, Heading, Stack, Text, useBreakpointValue} from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Heading,
+	HStack,
+	Image,
+	Stack,
+	Text,
+	useBreakpointValue,
+	useColorModeValue,
+} from "@chakra-ui/react";
 import {useTranslation} from "next-i18next";
 
 import Catalog from "@/src/components/Catalog/Catalog";
@@ -22,53 +32,61 @@ export default function Home(): JSX.Element {
 					top="94px"
 					width="100%"
 					left={useBreakpointValue({base: "0", lg: "15px"})}
-					bg="#805bd515"
+					bg={useColorModeValue("#805bd530", "#805bd515")}
 					borderTopLeftRadius="15px">
 					<Particles
 						style={{
 							height: "50vh",
 							minHeight: "500px",
-							filter: "blur(2px)",
+							filter: "blur(1px)",
 						}}
 					/>
 				</Box>
-				<Box textAlign="center" marginTop={{base: "50px", md: "100px"}} p={4}>
-					<Heading
-						as="h1"
-						size="3xl"
-						fontWeight="extrabold"
-						maxW="48rem"
-						mx="auto"
-						lineHeight="1.2"
-						letterSpacing="tight">
-						{t("common:page.home.header.title")}
-					</Heading>
-					<Text fontSize="xl" mt="4" maxW="xl" mx="auto">
-						{t("common:page.home.header.subtitle")}
-					</Text>
-				</Box>
-				<Stack
-					justify="center"
-					direction={{base: "column", md: "row"}}
-					mt="10"
-					mb="12"
-					spacing="4">
-					<Link href="/explore" passHref>
-						<Button
-							size="lg"
-							colorScheme="purple"
-							px="8"
-							fontWeight="bold"
-							fontSize="md">
-							{t("common:page.home.header.exploreButton")}
-						</Button>
-					</Link>
-					<Link href="/sell" passHref>
-						<Button size="lg" px="8" fontWeight="bold" fontSize="md">
-							{t("common:page.home.header.sellButton")}
-						</Button>
-					</Link>
-				</Stack>
+				<HStack w="full" mb="6">
+					<Box textAlign={{base: "center", xl: "left"}} p={4} w="full">
+						<Heading
+							as="h1"
+							size="3xl"
+							fontWeight="extrabold"
+							maxW="48rem"
+							lineHeight="1.2"
+							letterSpacing="tight"
+							mx={{base: "auto", xl: 0}}>
+							{t("common:page.home.header.title")}
+						</Heading>
+						<Text fontSize="xl" mt="4" maxW="xl" mx={{base: "auto", xl: 0}}>
+							{t("common:page.home.header.subtitle")}
+						</Text>
+						<Stack
+							w="full"
+							justify={{base: "center", xl: "flex-start"}}
+							direction={{base: "column", md: "row"}}
+							mt="10"
+							spacing="4">
+							<Link href="/explore" passHref>
+								<Button
+									size="lg"
+									colorScheme="purple"
+									px="8"
+									fontWeight="bold"
+									fontSize="md">
+									{t("common:page.home.header.exploreButton")}
+								</Button>
+							</Link>
+							<Link href="/sell" passHref>
+								<Button size="lg" px="8" fontWeight="bold" fontSize="md">
+									{t("common:page.home.header.sellButton")}
+								</Button>
+							</Link>
+						</Stack>
+					</Box>
+					<Image
+						pt={6}
+						d={{base: "none", xl: "block"}}
+						src="/assets/images/metaverse.png"
+						alt="metaverse"
+					/>
+				</HStack>
 				{!error && loading ? <Loading /> : <Catalog data={items} sorting={false} />}
 			</Box>
 		</Box>
