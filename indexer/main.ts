e1a91a9f747ff64f5e4458d81d8af0d2a90deadf
Marco Cesarato <marco.cesarato@accenture.com>
2022-03-25 22:22:13 +0100

    fix: add signint to indexer

--- indexer/main.ts
+++ indexer/main.ts
@@ -5,6 +5,20 @@ import logger from "./logger";
 dotenv.config();
 const {service} = require("./service");
 
+if (process.platform === "win32") {
+	var rl = require("readline").createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	rl.on("SIGINT", function () {
+		process.emit("SIGINT");
+	});
+}
+
+process.on("SIGINT", function () {
+	process.exit(0);
+});
+
 service().catch((error: Error) => {
 	logger.error(error.message);
 	process.exit(1);
