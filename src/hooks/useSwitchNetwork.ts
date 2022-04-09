da1e1566696786af84ff696f8c197cf7e3c15662
Marco Cesarato <marco.cesarato@accenture.com>
2022-04-09 12:04:18 +0200

    fix: network switcher provider

--- src/hooks/useSwitchNetwork.ts
+++ src/hooks/useSwitchNetwork.ts
@@ -2,24 +2,27 @@ import useWeb3 from "@hooks/useWeb3";
 import {networkConfigs} from "@utils/networks";
 
 export const useSwitchNetwork = () => {
-	const {web3} = useWeb3();
-
+	let {web3, enableWeb3} = useWeb3();
 	return async (chainId: string) => {
+		// @ts-ignore
+		let provider = web3?.currentProvider || (await enableWeb3())?.provider;
 		try {
-			await web3.provider.request({
+			await provider.request({
 				method: "wallet_switchEthereumChain",
 				params: [{chainId}],
 			});
 		} catch (error) {
 			if (error.code === 4902) {
 				try {
-					await web3.provider.request({
+					await provider.request({
 						method: "wallet_addEthereumChain",
 						params: [networkConfigs[chainId] || {}],
 					});
 				} catch (error) {
 					console.error(error.message);
 				}
+			} else {
+				console.error(error.message);
 			}
 		}
 	};
