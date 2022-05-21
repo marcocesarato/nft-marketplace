a9c4611bd732e9fe1ea066b649d3eed73f78a875
DIR\marco.cesarato <marco.cesarato@accenture.com>
2022-05-21 17:28:18 +0200

    fix: improve use dimension with observer

--- src/hooks/useDimensions.tsx
+++ src/hooks/useDimensions.tsx
@@ -1,6 +1,9 @@
 import {useCallback, useEffect, useState} from "react";
 
 const useDimensions = (ref) => {
+	const [observer, setObserver] = useState(null);
+	const [dimensions, setDimensions] = useState({width: 0, height: 0});
+
 	const getDimensions = useCallback(
 		() => ({
 			width: ref.current.offsetWidth,
@@ -9,14 +12,17 @@ const useDimensions = (ref) => {
 		[ref],
 	);
 
-	const [dimensions, setDimensions] = useState({width: 0, height: 0});
-
 	useEffect(() => {
 		const handleResize = () => {
 			setDimensions(getDimensions());
 		};
 
 		if (ref.current) {
+			observer?.disconnect();
+			const obs = new ResizeObserver(handleResize);
+			obs.observe(ref.current);
+
+			setObserver(obs);
 			setDimensions(getDimensions());
 		}
 
@@ -25,9 +31,10 @@ const useDimensions = (ref) => {
 			window.dispatchEvent(new Event("resize"));
 		}, 50);
 		return () => {
+			observer?.disconnect();
 			window.removeEventListener("resize", handleResize);
 		};
-	}, [getDimensions, ref]);
+	}, [getDimensions, observer, ref]);
 
 	return dimensions;
 };
