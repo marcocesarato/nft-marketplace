b2986de40106452150b5500526ec35119f86b130
DIR\marco.cesarato <marco.cesarato@accenture.com>
2022-05-26 10:22:23 +0200

    fix: add restricted permissions for galleryu and builder

--- src/pages/builder.tsx
+++ src/pages/builder.tsx
@@ -1,10 +1,20 @@
+import {useTranslation} from "next-i18next";
+
 import Content from "@components/Content";
 import GalleryBuilder from "@components/GalleryBuilder";
+import Header from "@components/Header";
 import {GalleryProvider} from "@contexts/Gallery";
+import useAccount from "@hooks/useAccount";
 import {getStaticPropsLocale} from "@utils/i18n";
 
 export const getStaticProps = getStaticPropsLocale;
 export default function Builder(): JSX.Element {
+	const {isAuthenticated} = useAccount();
+	const {t} = useTranslation();
+	if (!isAuthenticated)
+		return (
+			<Header title={t<string>("error:title")} subtitle={t<string>("error:auth.required")} />
+		);
 	return (
 		<Content>
 			<GalleryProvider>
--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -1,12 +1,22 @@
+import {useTranslation} from "next-i18next";
+
 import Content from "@components/Content";
 import Gallery from "@components/Gallery";
+import Header from "@components/Header";
 import Loading from "@components/Loading";
+import useAccount from "@hooks/useAccount";
 import useUser from "@hooks/useUser";
 import {getStaticPropsLocale} from "@utils/i18n";
 
 export const getStaticProps = getStaticPropsLocale;
 export default function MyGallery(): JSX.Element {
 	const {user, isLoading} = useUser();
+	const {isAuthenticated} = useAccount();
+	const {t} = useTranslation();
+	if (!isAuthenticated)
+		return (
+			<Header title={t<string>("error:title")} subtitle={t<string>("error:auth.required")} />
+		);
 
 	if (isLoading) {
 		return (
