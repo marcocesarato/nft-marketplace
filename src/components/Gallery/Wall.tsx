4f1c4125f95f7f3506833e767a76e2a03dea63d0
DIR\marco.cesarato <marco.cesarato@accenture.com>
2022-06-02 19:12:36 +0200

    fix: gallery wall colors

--- src/components/Gallery/Wall.tsx
+++ src/components/Gallery/Wall.tsx
@@ -33,8 +33,8 @@ export default function Wall({
 		<>
 			<Box
 				position={position}
-				color={color}
 				{...defaultWallAttributes}
+				color={color}
 				{...(texture ? convertAllStringToAttributes(texture.attributes) : {})}
 				{...props}
 			/>
