198feafe28af67ae3a5b70b351dbf53ba1a4d8d7
Marco Cesarato <marco.cesarato@accenture.com>
2022-09-27 13:06:04 +0200

    fix: map block memo

--- src/components/GalleryBuilder/Map.tsx
+++ src/components/GalleryBuilder/Map.tsx
@@ -9,13 +9,22 @@ import Block from "./Block";
 function Map(): JSX.Element {
 	const {schema} = useGallery();
 	const mapRef = useRef();
-	const {width: mapWidth, height, global} = useContainerDimensions(mapRef);
+	const {width, height, global} = useContainerDimensions(mapRef);
 	const planimetry = schema.getMap();
-	const maxRowWidth = useMemo(
-		() => Math.max(planimetry.width, planimetry.height),
-		[planimetry.width, planimetry.height],
-	);
-	const size = useMemo(() => mapWidth / maxRowWidth, [mapWidth, maxRowWidth]);
+	const mapHeight = useMemo(() => planimetry.height, [planimetry.height]);
+	const mapWidth = useMemo(() => planimetry.width, [planimetry.width]);
+	const maxRowWidth = useMemo(() => Math.max(mapWidth, mapHeight), [mapWidth, mapHeight]);
+	const size = useMemo(() => width / maxRowWidth, [width, maxRowWidth]);
+	const blocks = useMemo(() => {
+		return Array.from(Array(mapWidth).keys()).map((row) => (
+			<Box as="tr" key={`gallery-row-${row}`} height={size}>
+				{Array.from(Array(mapHeight).keys()).map((column) => {
+					const id = row * mapWidth + column;
+					return <Block key={`gallery-cell-${id}`} item={id} size={size} />;
+				})}
+			</Box>
+		));
+	}, [mapHeight, mapWidth, size]);
 
 	return (
 		<table
@@ -23,19 +32,10 @@ function Map(): JSX.Element {
 			style={{
 				flex: 2,
 				width: "100%",
-				height: mapWidth,
+				height: width,
 				maxWidth: (global?.innerHeight ?? height) - 150,
 			}}>
-			<tbody>
-				{Array.from(Array(planimetry.width).keys()).map((row) => (
-					<Box as="tr" key={`gallery-row-${row}`} height={size}>
-						{Array.from(Array(planimetry.height).keys()).map((column) => {
-							const id = row * planimetry.width + column;
-							return <Block key={`gallery-cell-${id}`} item={id} size={size} />;
-						})}
-					</Box>
-				))}
-			</tbody>
+			<tbody>{blocks}</tbody>
 		</table>
 	);
 }
