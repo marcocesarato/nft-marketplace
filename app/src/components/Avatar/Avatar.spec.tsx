da8a0b9a849c43904a7212f5872f0df4efafb347
Marco Cesarato <cesarato.developer@gmail.com>
2022-11-19 13:37:43 +0100

    test: add on avatar image size test

--- app/src/components/Avatar/Avatar.spec.tsx
+++ app/src/components/Avatar/Avatar.spec.tsx
@@ -9,4 +9,19 @@ describe("<Avatar />", () => {
 			.should("have.css", "width", `${size}px`)
 			.should("have.css", "height", `${size}px`);
 	});
+
+	it("mounts with ensImage", () => {
+		const size = 64;
+		cy.mount(
+			<Avatar
+				address="0x00000000000000000000000000000000000000f"
+				size={size}
+				ensImage="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+			/>,
+		)
+			.get("img")
+			.parent()
+			.should("have.css", "width", `${size}px`)
+			.should("have.css", "height", `${size}px`);
+	});
 });
