8a29f0278f6b9de609ef536f89d62ddfa8f44d0b
Marco Cesarato <cesarato.developer@gmail.com>
2022-11-20 21:03:18 +0100

    chore: add storybook theme

---
+++ app/.storybook/theme.js
@@ -0,0 +1,9 @@
+import {create} from "@storybook/theming";
+
+export default create({
+	base: "light",
+	brandTitle: "NFT MarketVerse",
+	brandUrl: "/",
+	//brandImage: '/icon-32x32.png',
+	brandTarget: "_self",
+});
