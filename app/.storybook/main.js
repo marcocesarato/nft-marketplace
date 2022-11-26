ac93912b9c7313ca60e6f552b861461ad7782ec3
Marco Cesarato <marco.cesarato@accenture.com>
2022-11-26 14:10:39 +0100

    chore: add storybook docs

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -20,6 +20,10 @@ const config = {
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@storybook/addon-interactions",
+		{
+			name: "@storybook/addon-docs",
+			options: {transcludeMarkdown: true},
+		},
 	],
 	framework: "@storybook/react",
 	features: {emotionAlias: false, interactionsDebugger: true},
